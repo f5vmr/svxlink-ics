52d31061864697b9b309fae0a6b8f9f2e7b2eeeb
Chris G4NAB_F5VMR <g4nab.ne63@gmail.com>
2026-02-17 21:01:14 +0000

    update  preinstall.sh

--- functions/ics_complete.sh
+++ functions/ics_complete.sh
@@ -9,7 +9,9 @@ ics_complete() {
     whiptail --title "Overlay Installation Complete" --msgbox "The overlays are now complete.\n\nThe next stage will be:\n./svxlink/install.sh\n\nafter the system has reinitiated.\n\nThe system will now reboot." 14 60
     
     # Initiate reboot
-    echo "Rebooting system..."
+    echo "Rebooting system... your next command will be: ./svxlink/install.sh" | sudo tee -a /var/log/install.log
+    echo "If you what an SvxReflector connection obtain a password from g4nab.ne63@gmail.com"
+    echo "If you wish to use EchoLink have your registration details to hand"
     sudo reboot
 }
 
