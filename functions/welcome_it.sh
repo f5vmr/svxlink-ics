78317f766eacf6f4df7f5b6a748bed3dccab9c02
Chris G4NAB_F5VMR <g4nab.ne63@gmail.com>
2026-02-17 16:40:18 +0000

    update ics - general settings.

--- functions/welcome_it.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-function welcome {
-message="Benvenuto nel mondo di SVXLink."
-whiptail --title "SVXLink" --msgbox "$message" 8 78
-
-}
-
--- functions/welcome_pt.sh
+++
@@ -1,7 +0,0 @@
-
-#!/bin/bash
-function welcome {
-message="Benvindo ao mundo do SVXLink."
-whiptail --title "SVXLink" --msgbox "$message" 8 78
-
-}
