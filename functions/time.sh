936fc3294f4c8982ce29c8f060d0ee99dcae3c70
Chris G4NAB_F5VMR <g4nab.ne63@gmail.com>
2026-03-10 15:14:23 +0000

    update

--- functions/time.sh
+++ functions/time.sh
@@ -2,15 +2,18 @@
 #function set timezone avoiding raspi-config
 set_timezone() {
 
-TZ=$(timedatectl list-timezones | \
-whiptail --title "Select Timezone" \
---menu "Choose your timezone" 20 60 10 \
-$(timedatectl list-timezones | awk '{print $1 " " ""}') \
+mapfile -t TZLIST < <(timedatectl list-timezones)
+
+MENU=()
+for tz in "${TZLIST[@]}"; do
+    MENU+=("$tz" "")
+done
+
+TZ=$(whiptail --title "Timezone Setup" \
+--menu "Select timezone" 20 70 12 \
+"${MENU[@]}" \
 3>&1 1>&2 2>&3)
 
-if [ $? -eq 0 ]; then
-    sudo timedatectl set-timezone "$TZ"
-    whiptail --msgbox "Timezone set to $TZ" 8 40
-fi
+[ -n "$TZ" ] && sudo timedatectl set-timezone "$TZ"
 
 }
